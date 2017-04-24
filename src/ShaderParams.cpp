159893c6081d28adf7232d542868ba3b46fe467a
jmacey <jmacey@bournemouth.ac.uk>
2017-04-24 10:56:21 +0100

    commit before refactor of MainWindow to UI based one

--- src/ShaderParams.cpp
+++ src/ShaderParams.cpp
@@ -1,10 +1,21 @@
 #include "ShaderParams.h"
 #include "ui_ShaderParams.h"
+#include "nodes/StyleCollection.hpp"
 
 ShaderParams::ShaderParams(QWidget *parent) :
   QFrame(parent),
   ui(new Ui::ShaderParams)
 {
+/*  const QtNodes::NodeStyle & nodeStyle = QtNodes::StyleCollection::nodeStyle();
+
+  QLinearGradient gradient(QPointF(0.0, 0.0),
+                           QPointF(2.0,2.0));
+
+  gradient.setColorAt(0.0,  nodeStyle.GradientColor0);
+  gradient.setColorAt(0.03, nodeStyle.GradientColor1);
+  gradient.setColorAt(0.97, nodeStyle.GradientColor2);
+  gradient.setColorAt(1.0,  nodeStyle.GradientColor3);
+*/
   ui->setupUi(this);
 }
 
